fbeca3fc1c199a2d2dddd7993d2a70527f90f2ca
moradmess <morad.elmesoudy@gmail.com>
2020-05-15 03:13:00 +0100

    ajouter un fichier layout

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/logo.jpg') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-social.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.css') }}">
+  <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+  <title>ZeCoursier</title>
+  <style type="text/css">
+    footer {
+      border: 1px solid rgb(232,232,232);
+      background-color: rgb(248,248,248);
+      border-radius: 5px;
+      padding-top: 10px;
+      padding-left: 5px;
+      padding-right: 5px;
+    }
+    #b {
+      float: right;
+    }
+    body {
+      font-family:Palatino Linotype;
+    }
+    nav {
+      font-size: 1.2em;
+    }
+
+.fa:hover {
+    opacity: 0.5;
+  }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <header>
+   <nav class="navbar navbar-default">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img width="30" style="margin-top: -5px;" src="{{ asset('img/logo.jpg') }}"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li id="accueil"><a href="{{ url('/') }}">Accueil<span class="sr-only">(current)</span></a></li>
+        <li id="propos"><a href="{{ url('/apropos') }}">A propos</a></li>
+        <li id="service"><a href="{{ url('/service') }}">Nos Service</a></li>
+        <li id="contact"><a href="{{ url('/contact') }}">Contactez-nous</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+</nav>
+<div class="row">
+    <img class="img-rounded center-block" style="width: 97%;" height="300" src="{{ asset('img/wajiha.jpg') }}" />
+  </div>
+</header>
+<hr />
+  
+
+@yield('content')
+
+<hr />
+<footer>
+  <div class="row" style="text-align: center;font-size: 1.2em">
+    <p class="col-sm-4">ICE: 002335495000074</p>
+    <p class="col-sm-4">Tous les droits réservés</p>
+    <p class="col-sm-4">ZeCoursier</p>
+  </div>
+</footer>
+  </div>
+</body>
+</html>
