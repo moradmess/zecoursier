e52da82d4125128164ebfadd1d28c449e894a64d
moradmess <morad.elmesoudy@gmail.com>
2020-05-16 04:58:37 +0100

    mofifier tous les données fichiers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,8 @@ document.getElementById('accueil').className = "active";
 <section class="col-sm-5" style="font-size: 1.2em;">
   <p><strong>ZeCoursier</strong>, une auto-entreprenariat est basée sur <strong>la livraison à domicile</strong> rapide de vos demandes que soient des marchandises, des medicaments, repats, documents...à Fès aussi à ses environs</p>
     <p>Zakaria Elmesoudy, le créateur de <strong>Zecoursier</strong> un jeune entrepreneur qui habite à la ville de Fes, motivé. J'ai toujours l'ambition de rendre mon service le plus bon possible, et de travailler sur les lacunes et les défauts afin de satisfaire nos clients.</p>
-<img class="hidden-sm img-responsive img-rounded" src="{{ asset('img/banner1.jpg') }}" style="width:100%;margin-bottom: 15px;" alt="banner1">
+<img class="hidden-sm img-responsive img-rounded" src="{{ secure_asset('img/banner1.jpg') }}" style="width:100%;margin-bottom: 15px;" alt="banner1">
+<!--<img class="hidden-sm img-responsive img-rounded" src="{{ asset('img/banner1.jpg') }}" style="width:100%;margin-bottom: 15px;" alt="banner1">-->
 </section>
 <aside class="col-sm-7">
     <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fzecoursier.officiel%2Fvideos%2F2588580877915913%2F&show_text=0&width=560" scrolling="no" frameborder="0" height="470" allowTransparency="true" allowFullScreen="true" style="width: 100%;border: none;overflow: hidden;"></iframe>
@@ -113,14 +114,14 @@ document.getElementById('accueil').className = "active";
 </aside>
 <aside class="col-sm-4" style="margin-top: 20px">
     <a href="https://www.facebook.com/HasniPromotion" onclick="window.open(this.href); return false;">
-      <img class="img-responsive center-block img-thumbnail" src="{{ asset('img/client-3.jpg') }}"  width="250" height="250" alt="Hasni">
+      <img class="img-responsive center-block img-thumbnail" src="{{ secure_asset('img/client-3.jpg') }}"  width="250" height="250" alt="Hasni">
       <!--<img class="img-responsive center-block img-thumbnail" src="{{ asset('img/client-3.jpg') }}"  width="250" height="250" alt="Hasni">-->
     </a>
 </aside>
    </div> 
    <br />
    <section class="row">
-        <img src="{{ asset('img/banner2.jpg') }}" class="img-responsive center-block img-rounded" alt="banner2">
+        <img src="{{ secure_asset('img/banner2.jpg') }}" class="img-responsive center-block img-rounded" alt="banner2">
         <!--<img src="{{ asset('img/banner2.jpg') }}" class="img-responsive center-block img-rounded" alt="banner2">-->
    </section>
 </div>
