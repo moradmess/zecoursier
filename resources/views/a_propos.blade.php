a49fbd1b5a5bb27e15f11eb95f19e9cde26f567d
moradmess <morad.elmesoudy@gmail.com>
2020-05-17 22:57:00 +0100

    modifier dans la page apropos

--- resources/views/a_propos.blade.php
+++ resources/views/a_propos.blade.php
@@ -8,7 +8,7 @@ document.getElementById('propos').className = "active";
 	<!--<img margin-bottom: 10px" width="100%" class="img-rounded img-responsive" src="{{ secure_asset('img/zecoursier_logo.jpg') }}" alt="ZeCoursier">-->
 	<img margin-bottom: 10px" width="100%" class="img-rounded img-responsive" src="{{ asset('img/zecoursier_logo.jpg') }}" alt="ZeCoursier">
 	<aside class="col-sm-6 col-sm-offset-3" style="text-align: justify; text-align-last: center;font-size: 1.4em;margin-top: 25px">
-		<p><strong>ZeCOURSIER</strong> est une auto-entreprenariat, son rôle principal est <strong>le stockage, la livraison à domicile et vous pouvez suivre</strong> l'état de vos colis que ça soit une marchandise, produit, ..., etc.</p>
+		<p><strong>ZeCOURSIER</strong> est une auto-entreprenariat, son rôle principal est <strong>le stockage et la livraison à domicile, ainsi vous pouvez suivre</strong> l'état de vos colis que ça soit une marchandise, produit, ..., etc.</p>
 	<p>Donc si vous  êtes un e-commercial ou commercial chargé des demandes sur internet de votre client <strong>à domicile</strong>, vous n'hésitez pas à choisir notre service. <strong>ZeCOURSIER</strong> est le bon choix pour vous, et nous aurons de la fierté de vous servir.</p>
 	</aside>
 </section>
